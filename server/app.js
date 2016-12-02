db4dac6dceba02ccec718509f710d2c70e1ea947
Luke Schlangen <lukeschlangen@gmail.com>
2016-12-01 19:51:59 -0600

    Moving private data to routes folder

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 var decoder = require('./modules/decoder');
+var privateData = require('./routes/private-data');
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
@@ -10,7 +11,7 @@ app.use(bodyParser.json());
 // Decodes the token in the request header and attaches the decoded token to the request.
 app.use(decoder.token);
 
-/* Whatever you do below is protected by your authentication.
+/* Whatever you do below this is protected by your authentication.
 WARNING: So far you are returning secret data to ANYONE who is logged in.
 There is still more work to be done if you want to implement roles.
 No authorization has been completed yet in this branch.
@@ -18,10 +19,7 @@ You can use req.decodedToken and some logic to do that.
 Other branches in the nodeFire repository show how to do that. */
 
 // This is the route for your secretData. The request gets here after it has been authenticated.
-app.get("/privateData", function(req, res){
-  console.log(req.decodedToken); // Here you can see the information firebase gives you about the user
-  res.send("Secret DATA!!! You got it!!! Great work " + req.decodedToken.name + "!!!");
-});
+app.use("/privateData", privateData);
 
 var portDecision = process.env.PORT || 5000;
 
