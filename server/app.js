a692d983c414da4beeebc60c0174d8acd4104030
Luke Schlangen <lukeschlangen@gmail.com>
2016-12-01 19:37:45 -0600

    Fixing comments

--- server/app.js
+++ server/app.js
@@ -10,6 +10,12 @@ app.use(bodyParser.json());
 // Decodes the token in the request header and attaches the decoded token to the request.
 app.use(decoder.token);
 
+/* Whatever you do in below is protected by your authorization.
+WARNING: So far you are returning secret data to ANYONE who is logged in
+there is still more work to be done if you want to implement roles.
+You can use the decodedToken and some logic to do that.
+Other branches in the nodeFire repository show how to do that. */
+
 // This is the route for your secretData. The request gets here after it has been authenticated.
 app.get("/privateData", function(req, res){
   console.log(req.decodedToken); // Here you can see the information firebase gives you about the user
--- server/modules/decoder.js
+++ server/modules/decoder.js
@@ -9,16 +9,14 @@ admin.initializeApp({
 verify it against our private_key, and then we return the decodedToken */
 var tokenDecoder = function(req, res, next){
   admin.auth().verifyIdToken(req.headers.id_token).then(function(decodedToken) {
-    /* Whatever you do in here is protected by your authorization.
-    WARNING: So far you are returning secret data to ANYONE who is logged in
-    there is still more work to be done if you want to implement roles
-    You can use the decodedToken and some logic to do that. */
+    // Adding the decodedToken to the request so that downstream processes can use it
     req.decodedToken = decodedToken;
     next();
   })
   .catch(function(error) {
     // If the id_token isn't right, you end up in this callback function
-    // Here we are simply returning a string response, but a forbidden error may be better
+    // Here we are returning a forbidden error
+    console.log('User token could not be verified');
     res.send(403);
   });
 }
