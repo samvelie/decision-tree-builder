1fbcd040c04b61986cb4eabc7c15bf2b6a11728c
Luke Schlangen <lukeschlangen@gmail.com>
2016-12-09 17:31:35 -0600

    fixing error appearring on initial load

--- server/modules/decoder.js
+++ server/modules/decoder.js
@@ -9,17 +9,23 @@ admin.initializeApp({
 verify it against our firebase service account private_key.
 Then we add the decodedToken */
 var tokenDecoder = function(req, res, next){
-  admin.auth().verifyIdToken(req.headers.id_token).then(function(decodedToken) {
-    // Adding the decodedToken to the request so that downstream processes can use it
-    req.decodedToken = decodedToken;
-    next();
-  })
-  .catch(function(error) {
-    // If the id_token isn't right, you end up in this callback function
-    // Here we are returning a forbidden error
-    console.log('User token could not be verified');
+  if (req.headers.id_token) {
+    admin.auth().verifyIdToken(req.headers.id_token).then(function(decodedToken) {
+      // Adding the decodedToken to the request so that downstream processes can use it
+      req.decodedToken = decodedToken;
+      next();
+    })
+    .catch(function(error) {
+      // If the id_token isn't right, you end up in this callback function
+      // Here we are returning a forbidden error
+      console.log('User token could not be verified');
+      res.sendStatus(403);
+    });
+  } else {
+    // Seems to be hit when chrome makes request for map files
+    // Will also be hit when user does not send back an idToken in the header
     res.sendStatus(403);
-  });
+  }
 }
 
 module.exports = { token: tokenDecoder };
